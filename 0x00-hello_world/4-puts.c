db3af1c1cbd9703e67be1eebca40851aec2ee1c6
KJGZULU <kondwamnzulu@gmail.com>
2023-06-16 01:54:22 -0700

    C Programing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - printing Programming is like building a multilingual puzzle
- *Return: 0 (Success)
+ *Return: 0(Success)
  */
 int main(void)
 {
 puts("Programing is like building a multilingual puzzle\n");
-return 0;
+return (0);
 }
 
 
