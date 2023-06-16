29805d07b7f953158b0d665dbc0ca13cc6ef15f8
KJGZULU <kondwamnzulu@gmail.com>
2023-06-16 02:18:16 -0700

    C Programing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - Printing a line using printf
  * Return: 0(success)
-*/
+ */
 int main(void)
 {
-printf(with proper grammar, but the outcome is a piece of art, \n);
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
