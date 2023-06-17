8edb86b518b9c8964838d2a03911d00f8d397483
KJGZULU <kondwamnzulu@gmail.com>
2023-06-17 05:44:14 -0700

    c variables if els and while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /*
-*main - Entry point
-*Return: 0 (success)
+* main - Entry point
+* Return: 0 (success)
 */
 int main(void)
 {
@@ -16,5 +16,6 @@ else if (n == 0)
 printf("%d is zero\n", n);
 else
 printf("%d is negative\n", n);
+printf("\n")
 return (0);
 }
