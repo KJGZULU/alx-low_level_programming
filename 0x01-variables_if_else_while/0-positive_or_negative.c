0d26596a19f0f0d09557394bb8ce35346b0eeff0
KJGZULU <kondwamnzulu@gmail.com>
2023-06-17 05:27:32 -0700

    c variables if els and while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,21 +2,19 @@
 #include <time.h>
 #include <stdio.h>
 /*
- *main - Positive anything is better than negative nothing
- *Return: 0 (success)
- */
-
+*main - Positive anything is better than negative nothing
+*Return: 0 (success)
+*/
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n>0)
-		printf("%d is negative\n", n);
-	else if (n==0)
-		printf("%d is zero\n", n);
-	else 
-		printf("%d is negative\n", n);
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n>0)
+printf("%d is negative\n", n);
+else if (n==0)
+printf("%d is zero\n", n);
+else 
+printf("%d is negative\n", n);
+return (0);
 }
