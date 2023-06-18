915cfd3cfada32784377e9bf8f6b2a35458b7e79
KJGZULU <kondwamnzulu@gmail.com>
2023-06-18 07:45:01 -0700

    C variables if else and while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * main - entry point
- * return: 0 (success)
+ * Main - Entry point
+ * Return: 0 (success)
  */
 int main(void)
 {
 int d, p, q;
 for (d = '0'; d < '9'; d++)
 {
-for (p = d +1; p <= '9'; p++)
+for (p = d + 1; p <= '9'; p++)
 {
-for (q = p +1; q <= '9'; q++)
+for (q = p + 1; q <= '9'; q++)
 {
 if ((p != d) != q)
 {
