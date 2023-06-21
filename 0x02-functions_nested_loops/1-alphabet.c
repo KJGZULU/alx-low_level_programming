bda83988cf5a5542aacdbab23b805331f5d40434
KJGZULU <kondwamnzulu@gmail.com>
2023-06-21 06:23:41 -0700

    C - functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+char k;
+for (k = 'a'; k <= 'z'; k++)
+{
+_putchar(k);
+}
+_putchar('\n');
+}
