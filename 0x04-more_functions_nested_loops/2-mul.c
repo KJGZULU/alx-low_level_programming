939eeeaa90332027df4a17ee2784c9c0e022c67f
KJGZULU <kondwamnzulu@gmail.com>
2023-06-22 03:21:03 -0700

    C more functions nested and loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiple of two integers
+ * @t: The first number
+ * @k: The second number
+ * Return: the results
+ */
+int mul(int t, int k)
+{
+return (t * k);
+}
