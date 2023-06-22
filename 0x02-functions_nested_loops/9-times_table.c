9d4c5cf784b7ef9692dc006258b63d679de3ea3a
KJGZULU <kondwamnzulu@gmail.com>
2023-06-21 18:45:09 -0700

    C functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 int a, b, c, d, e;
 for (a = 0; a <= 9; a++)
 {
-for (b = 0; b <=9; b++)
+for (b = 0; b <= 9; b++)
 {
 c = a * b;
 if (c > 9)
@@ -18,7 +18,7 @@ e = (c - d) / 10;
 _putchar(44);
 _putchar(32);
 _putchar(e + '0');
-_putchar(d +'0');
+_putchar(d + '0');
 }
 else
 {
