606935ac110806c3f2e48b2bc88af836f872e9b8
KJGZULU <kondwamnzulu@gmail.com>
2023-06-21 18:53:25 -0700

    C functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add -  a function that adds two integers and returns the result.
+ * @n1 - 1st computed number
+ * @n2 - 2nd computed number
+ * Return: integer
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
