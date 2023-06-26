fa6b89154e48510f02a6570cd180dc417a2095ee
KJGZULU <kondwamnzulu@gmail.com>
2023-06-26 02:59:55 -0700

    0. 98 Battery st

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * swap_in - swaps th value of int
+ * @a: the first int to be swapped
+ * @b: the second int to be swapped
+ * Return: zero
+ */
+void swap_int(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+}
