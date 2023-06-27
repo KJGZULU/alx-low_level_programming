c0bfb738e35052b79714263a4b32e1a6d465a0ab
KJGZULU <kondwamnzulu@gmail.com>
2023-06-27 07:17:48 -0700

    i do not fear computers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _atoi - convert a string to an integer
- * @s: pointer to convert
- * Return: an integer
+ * @s: The pointer to convert
+ * Return: A integer
  */
 int _atoi(char *s)
 {
